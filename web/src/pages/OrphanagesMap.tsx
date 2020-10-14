8b023f221af7d5a567add02aa63ca7172a374553
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-10-14 10:37:21 -0300

    initial commit projeto happy

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css'
+
+import '../styles/pages/orphanages-map.css'
+import mapMarker from '../images/map-marker.svg';
+
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Angatuba</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+            
+            <Map 
+                center={[-23.495532, -48.4282114]}
+                zoom={15}
+                style={{ width: '100%', height: '100%'}}>
+
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    );
+}
+
+export default OrphanagesMap;
