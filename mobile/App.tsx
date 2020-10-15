5e99174f310ab885b2f64706f81a5b39b02b5aad
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-10-15 17:18:02 -0300

    adicionando markerIcon no map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import  MapView  from 'react-native-maps';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView 
-      style={styles.map} 
-      initialRegion={
-        {
-          latitude: -23.495532, 
-          longitude: -48.4282114,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }
-      }/>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={
+          {
+            latitude: -23.495532,
+            longitude: -48.4282114,
+            latitudeDelta: 0.008,
+            longitudeDelta: 0.008,
+          }
+        } >
+          <Marker 
+            icon={mapMarker}
+            calloutAnchor={{
+              x: 2.55,
+              y: 1
+            }}
+            coordinate={{
+              latitude: -23.495532,
+              longitude: -48.4282114,
+            }}
+          >
+            <Callout tooltip onPress={() => {}}>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}>Lar das meninas</Text>
+              </View>
+            </Callout>
+          </Marker>
+        </MapView>
     </View>
   );
 }
@@ -25,7 +46,21 @@ const styles = StyleSheet.create({
   },
 
   map: {
-    width: Dimensions.get('window').width, 
-    height: Dimensions.get('window').height, 
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: '#DCDCDC',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+
+  calloutText: {
+    color: '#0089A5',
+    fontSize: 14
   }
 });
